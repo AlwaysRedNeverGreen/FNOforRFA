1929058ad821bfb9477889e949d058ffd9e750b4
AlwaysRedNeverGreen <masdaqb@gmail.com>
2024-03-24 00:53:44 +0800

    cleaned up

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ import trainer2 as tr2
 import torch
 
 input_seq_len=1
-epochs = 20
-prediction_length = 5
-file_path = 'Data/CASE01_DATA.mat'
-model_path = 'trained_model_multiple1_5out.pth'
+epochs = 5
+prediction_length = 1
+file_path = 'Data/CASE02_DATA.mat'
+model_path = 'Models/DS2_Multi_10out.pth'
 
 data_variables = ld.loadData(file_path) #Load the data from the file
-train_loader, test_loader,last_timestep = sp.create_datasets(data_variables, input_seq_len=input_seq_len, prediction_len=prediction_length, train_ratio=0.5, batch_size=1) #Create the datasets
-#vd.print_test_loader_contents(test_loader) #View the contents of the test loader
-#print(data_variables)
-#tr.training(train_loader,test_loader,input_seq_len,epochs,model_path, prediction_length) #Train the model (this will save the model as well)
-loaded_model = torch.load(model_path) #Load the model (this will be used for evaluation)
+#dv.createAnimation(data_variables, 'caseW5') #Create an animation of the data from the dataset
 
+train_loader, test_loader,last_timestep = sp.create_datasets(data_variables, input_seq_len=input_seq_len, prediction_len=prediction_length, train_ratio=0.2, batch_size=1) #Create the datasets
+tr.training(train_loader,test_loader,input_seq_len,epochs,model_path, prediction_length,last_timestep) #Train the model (this will save the model as well)
+
+loaded_model = torch.load(model_path) #Load the model (this will be used for evaluation)
 me.eval(test_loader,loaded_model,input_seq_len,last_timestep,prediction_len=prediction_length) #Evaluate the model
