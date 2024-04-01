7fe984aebcb6991320b566e257c327060b8c9c72
AlwaysRedNeverGreen <masdaqb@gmail.com>
2024-04-01 12:26:05 +0800

    Automated some filenaming conventions and added rmse dict to store rmse's in a dictionary to be able to put them in a .csv. Also added differences dictionary that holds the difference between true and predicted value, so that it can be used to visualize the error

--- fullDatasetEval.py
+++ fullDatasetEval.py
@@ -6,55 +6,61 @@ import dataVisualization as dv
 import loadData as ld
 import continuousDataLoaders as cdl
 import viewData as vd
+import Graphing as gp
 
-def eval(combined_loader, loaded_model, input_seq_len, last_timestep,model_name,data_name, prediction_len=1, interval=5):
-    predictions = [] # List to store the model's predictions
-    ground_truths = [] # List to store the ground truth temperature distributions
-    timesteps = [] # List to store the timesteps for each prediction
-    results_dict = {} # Dictionary to store the results for each timestep
+def eval(device,combined_loader, loaded_model, input_seq_len, last_timestep,model_name,case_name, prediction_len=1, interval=5):
+    ground_truths = {} # List to store the ground truth temperature distributions
+    differences = {} # List to store the differences between the ground truth and the model's predictions
+    predictions = {} # Dictionary to store the results for each timestep
     total_loss = [] # Keep track of the loss at each time step
-    
+    rmse_dict = {}
     current_timestep = last_timestep
 
     with torch.no_grad():
         for batch in combined_loader:
             i=0
-            x_test, y_true = batch['x'], batch['y']
+            x_test, y_true = batch['x'].to(device), batch['y'].to(device)
             if i == 0:
                 y_pred = loaded_model(x_test)
-                print(f'xtest {current_timestep}')# \n {x_test}')
-                results_dict[f'Original input T{current_timestep}'] = x_test
+                
+                predictions[f'Original input T{current_timestep}'] = x_test
+                ground_truths[f'Ground Truth T{current_timestep}'] = x_test
+                differences[f'Difference T{current_timestep}'] = x_test - x_test
+                
                 current_timestep +=5
+                
             for t in range(y_true.size(1)):
-                #print("ypred\n:",y_pred)
-                #print("ytrue\n:",y_true[:, t, :, :])
-                results_dict[f'Prediction T{current_timestep}'] = y_pred
+                predictions[f'Prediction T{current_timestep}'] = y_pred.squeeze()
+                ground_truths[f'Ground Truth T{current_timestep}'] = y_true[:, t, :, :].squeeze()
+                
                 loss = (y_true[:, t, :, :] - y_pred) ** 2
+                differences[f'Difference T{current_timestep}'] = loss.squeeze()
                 
                 mse = torch.mean(loss)
                 rmse = torch.sqrt(mse)
+                rmse_dict[f'T{current_timestep}'] = rmse.item()
                 total_loss.append(rmse.item())
                 print(f"RMSE at time step {current_timestep}: {rmse.item()}")
+                
                 current_timestep += 5
-                #print("input ypred\n:",y_pred)
-                y_pred = loaded_model(y_pred)
-                #print("output ypred\n:",y_pred)
+                y_pred = loaded_model(y_pred) # Use the model's output as the input for the next timestep
                 i+=1
                                
-    print(f"Mean of the error list: {sum(total_loss) / len(total_loss)}")        
-    #print("results_dict",results_dict)
-    dv.createAnimation(results_dict, f'{data_name}',model_name) 
+    print(f"Mean of the error list: {sum(total_loss) / len(total_loss)}")
+    gp.append_results_to_csv(model_name, rmse_dict, f'eval_results_{case_name}.csv')
+    #dv.createAnimationComparison(ground_truths, predictions, differences, case_name, model_name)
 
-def evalParams(combined_loader, loaded_model, input_seq_len, last_timestep,model_name,data_name, prediction_len=1, interval=5):
-    predictions = [] # List to store the model's predictions
-    ground_truths = [] # List to store the ground truth temperature distributions
-    timesteps = [] # List to store the timesteps for each prediction
-    results_dict = {} # Dictionary to store the results for each timestep
-    total_loss = [] # Keep track of the loss at each time step
-    k,w,sig = 3,3,5
+def evalParams(device,combined_loader, loaded_model, input_seq_len, last_timestep,model_name,case_name, prediction_len=1, interval=5):
+    ground_truths = {} # List to store the ground truth temperature distributions
+    differences = {} # List to store the differences between the ground truth and the model's predictions
+    predictions = {} # Dictionary to store the results for each timestep
+    total_loss = [] # Keep track of the loss at each time ste
+    rmse_dict = {}
     current_timestep = last_timestep
+    
+    k,w,sig = 3,3,2
     params_tensor = torch.tensor([k, w, sig]).view(1, 3, 1, 1).expand(-1, -1, 101, 101)
-    params_tensor = params_tensor.to('cpu')
+    params_tensor = params_tensor.to(device)
     
     with torch.no_grad():
         for batch in combined_loader:
@@ -63,17 +69,20 @@ def evalParams(combined_loader, loaded_model, input_seq_len, last_timestep,model
             if i == 0:
                 x_with_params = torch.cat((x_test, params_tensor), dim=1)
                 y_pred = loaded_model(x_with_params)
-                results_dict[f'Original input T{current_timestep}'] = x_test
-                
+                predictions[f'Original input at T{current_timestep}'] = x_test
+                ground_truths[f'Ground Truth at T{current_timestep}'] = x_test
+                differences[f'Error at T{current_timestep}'] = x_test - x_test
                 current_timestep +=5
             for t in range(y_true.size(1)):
-                #print("ypred\n:",y_pred)
-                #print("ytrue\n:",y_true[:, t, :, :])
-                results_dict[f'Prediction T{current_timestep}'] = y_pred
+                predictions[f'Prediction at T{current_timestep}'] = y_pred.squeeze()
+                ground_truths[f'Ground Truth at T{current_timestep}'] = y_true[:, t, :, :].squeeze()
+                
                 loss = (y_true[:, t, :, :] - y_pred) ** 2
+                differences[f'Error at T{current_timestep}'] = loss.squeeze()
                 
                 mse = torch.mean(loss)
                 rmse = torch.sqrt(mse)
+                rmse_dict[f'T{current_timestep}'] = rmse.item()
                 total_loss.append(rmse.item())
                 print(f"RMSE at time step {current_timestep}: {rmse.item()}")
                 current_timestep += 5
@@ -82,22 +91,27 @@ def evalParams(combined_loader, loaded_model, input_seq_len, last_timestep,model
                 y_pred = loaded_model(y_with_params)
                 i+=1
                                
-    print(f"Mean of the error list: {sum(total_loss) / len(total_loss)}")        
-    #print("results_dict",results_dict)
-    dv.createAnimation(results_dict, f'{data_name}',model_name) 
-
+    print(f"Mean of the error list: {sum(total_loss) / len(total_loss)}")
+    gp.append_results_to_csv(model_name, rmse_dict, f'eval_results_{case_name}.csv')
+    #dv.createAnimationComparison(ground_truths, predictions, differences, case_name, model_name)
+        
 input_seq_len=1
 prediction_length = 100
-
 #file_path = 'Data/Original/CASE01_DATA.mat'
 #file_path = 'Data/K/k3.mat'
-file_path = 'Data/SIG/sig5.mat'
+file_path = 'Data/SIG/sig4.mat'
 #file_path = 'Data/W/w3.mat'
-model_path = f'Models/k1245params_64.pth'
+case_name = file_path.split('/')[-1].split('.mat')[0]
+
+#model_path = f'Models/TrainedOn_K1,2,4,5_Params_64Res.pth'
+model_path = f'Models/TrainedOn_K1,2,4,5_NoParams_64Res.pth'
+model_name = model_path.split('/')[-1].split('.')[0]
 
 data_variables = ld.loadData(file_path) #Load the data from the file
 loader = cdl.create_single_dataset(data_variables,1)
-#vd.print_test_loader_contents(loader)
-loaded_model = torch.load(model_path, map_location=torch.device('cpu'))
-evalParams(loader,loaded_model,input_seq_len,5,"k1245params_64 ","w3",prediction_len=prediction_length)
-#eval(loader,loaded_model,input_seq_len,5,"lowTest100k1245noparams","w3",prediction_len=prediction_length)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+loaded_model = torch.load(model_path, map_location=torch.device(device))
+
+#evalParams(device,loader,loaded_model,input_seq_len,5, model_name,case_name,prediction_len=prediction_length)
+eval(device,loader,loaded_model,input_seq_len,5,model_name,case_name,prediction_len=prediction_length)
